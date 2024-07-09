8a771ad0e79ebd9a676dd8973cd968e62778a1ef
winifredogbeiwi <imadeogbeiwi@gmail.com>
2024-07-09 12:16:41 +0200

    increase database selection timeout

--- config/db.js
+++ config/db.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
-export const database = mongoose.connect(process.env.MONGODBURI);
-
-
+export const database = mongoose.connect(process.env.MONGODBURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 50000,
+});
